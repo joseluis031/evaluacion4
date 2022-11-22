78ffeb890e8440b0fd5cb47fd511258cf2f6eb05
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-11-22 19:42:01 +0100

    creo el arbol

--- ejercicio1.py
+++ ejercicio1.py
@@ -17,3 +17,29 @@ def buscar(raiz,clave):
         else:
             pos = buscar(raiz.der, clave)
     return pos
+
+
+import heapq
+
+def make_tree(probs):
+    q = []
+    # Agregamos todos los símbolos a la pila
+    for ch,pr in probs.items():
+        # La fila de prioridad está ordenada por
+        # prioridad y profundidad
+        heapq.heappush(q,(pr,0,ch))
+
+    # Empezamos a mezclar símbolos juntos
+    # hasta que la fila tenga un elemento
+    while len(q) > 1:
+        e1 = heapq.heappop(q) # El símbolo menos probable
+        e2 = heapq.heappop(q) # El segundo menos probable
+        
+        # Este nuevo nodo tiene probabilidad e1[0]+e2[0]
+        # y profundidad mayor al nuevo nodo
+        nw_e = (e1[0]+e2[0],max(e1[1],e2[1])+1,[e1,e2])
+        heapq.heappush(q,nw_e)
+    return q[0] # Devolvemos el arbol sin la fila
+
+a = make_tree({'a':0.2,'f':0.17,"1":0.13,"3":0.21,"0":0.05,"M":0.09,"T":0.15})
+print(a)
