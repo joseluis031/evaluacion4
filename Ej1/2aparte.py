09a0f0b9a2aefdc22d4d403be793d08751410482
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-11-23 00:35:25 +0100

    funcion que comprime

---
+++ Ej1/2aparte.py
@@ -0,0 +1,12 @@
+def comprimir(texto):
+    separador = ""
+    contador = 0
+    
+    for letra in texto:
+        separador += #busca que codigo del que tengo debo poner aqui
+        if contador +1 < len(texto):
+            separador += contador
+        contador +=1
+    
+    return separador
+            
