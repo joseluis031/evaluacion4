56ca56982960ba367df81b663f8bfafbec3388ea
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-11-23 22:49:08 +0100

    constructor arbol

---
+++ Ej2/codigo_arbol.py
@@ -0,0 +1,9 @@
+class NodoArbol:
+       
+   def __init__(self,info,texto):
+        self.izq = None
+        self.der = None
+        self.info = info
+        self.altura = 0
+        self.texto = texto
+    
