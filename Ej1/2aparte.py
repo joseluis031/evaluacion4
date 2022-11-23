d50344d73f9bd8e86c6b06961d7a037a0233d8e6
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-11-23 19:57:22 +0100

    probando

--- Ej1/2aparte.py
+++ Ej1/2aparte.py
@@ -1,12 +1,16 @@
+dict_binario = {"a":00,"3":"01","1":100,"0":1010,"M":1011,"T":110,"f":111}
+dict_binario
 def comprimir(texto):
     separador = ""
     contador = 0
     
     for letra in texto:
-        separador += #busca que codigo del que tengo debo poner aqui
+        separador += dict_binario       
         if contador +1 < len(texto):
             separador += contador
         contador +=1
     
     return separador
+c="skodf"
+comprimir("f")
             
