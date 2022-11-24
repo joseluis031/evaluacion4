1286ffa50d31027c7afd429a20516aedf36c485d
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-11-24 16:22:30 +0100

    funcion decodificar

--- Ej1/2apartefinal.py
+++ Ej1/2apartefinal.py
@@ -1,8 +1,21 @@
+separador = " "
+
 def codificar(texto):
     binario = " ".join(format(ord(i), "b")for i in texto) #b implica formato binario
     print("La codificacion del texto es la siguiente:")
     return binario
 
+def decodificar(texto2):
+    texto_vacio = ""
+    for i in texto2.split(separador):
+        texto_vacio += codificar(i)
+    return texto_vacio
+
 a = input("¿que deseas codificar?")
 b = codificar(a)
 print(b)
+
+c = input("¿que deseas decodificar(en binario por favor?")
+d = decodificar(c)
+print(d)
+
