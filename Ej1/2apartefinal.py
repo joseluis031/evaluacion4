99fdf1e5c78e8e5121fe1611852f656cf32b6aea
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-11-25 13:25:34 +0100

    correccion

--- Ej1/2apartefinal.py
+++ Ej1/2apartefinal.py
@@ -5,17 +5,22 @@ def codificar(texto):
     print("La codificacion del texto es la siguiente:")
     return binario
 
+def decimal(binario):
+    valor = int(binario, 2)
+    return chr(valor) # recibe un número y devuelve su representación como carácter
+
+
 def decodificar(texto2):
     texto_vacio = ""
     for i in texto2.split(separador):
-        texto_vacio += codificar(i)
+        texto_vacio += decimal(i)
     return texto_vacio
 
 a = input("¿que deseas codificar?")
 b = codificar(a)
 print(b)
 
-c = input("¿que deseas decodificar(en binario por favor?")
+c = input("¿que deseas decodificar(en binario por favor)?")
 d = decodificar(c)
 print(d)
 
