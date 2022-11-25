470173611cea48d55cca18bc3f8224f57401a1ca
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-11-25 18:29:45 +0100

    ej3

---
+++ Ej3/ejercicio3.py
@@ -0,0 +1,16 @@
+""" Explicacion ejercicio 3
+--Empezaria creando la clase grafo con su constructor
+
+--utilizaria las funciones de insertar arista e insertar vertice
+---y le daria a cada vertice el valor de cada pais
+---con la funcion de insertar aristas uniria cada vertice con los otros de su mismo tipo
+
+--para el arbol de expansion minimo se refiere al mas corto/eficiente
+---utilizaria el algoritmo de dijkstra
+
+--funcion que al darle yo los grafos, me dijera si hay algun pais que tuviera los 2 tipos de maravillas
+---lo haria mediante bucles y lo almacenaria en listas
+--utilizaria la misma funcion pero utilizando otro "filtro" para ver si hay algun pais
+---que tenga mas de 1 maravilla
+
+"""
