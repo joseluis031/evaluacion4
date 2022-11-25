12578c5bd90568f6b20e5324da395fd14585e67a
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-11-25 18:01:48 +0100

    explicacion ejercicio 2

--- Ej2/ejercicio2.py
+++ Ej2/ejercicio2.py
@@ -1,8 +1,18 @@
 """ explicacion ejercicio 2
+Para empezar crearia una clase Arbol
+--Su constructor
+--Crearia una fucion que leyera el csv pokemon y
+---me creara los arboles que necesito para resolver las preguntas
+-----crearia funciones para indicar las caracteristicas mas optimas para cada arbol(altura)
 
+--crearia una nueva funcion para buscar clave en el arbol(funcion buscar de los apuntes)
+--funcion que pueda filtrar ciertas caracteristicas de los pokemon(para apartado c)
 
+--utilizaria la funcion postorder para un listado ascendente y la funcion por_nivel para listado por niveles
 
+--haria una funcion que me creara una lista de aquellos pokemon que al compararlos con os que me pide el ejercicio, sean mas debiles
 
-
+--crearia una funcion que en la cual leyera todos los diferentes tipos de pokemon
+--- y me los almacenara en un diccionario segun su tipo
 
 """
