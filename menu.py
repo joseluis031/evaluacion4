f0f7b31b2cfb561f750546f894f3dcd9d801c252
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-11-25 14:23:24 +0100

    cambios

--- menu.py
+++ menu.py
@@ -43,31 +43,16 @@ def iniciar():
 
         elif opcion == '2':
             print("Enunciado del ejercicio 2...\n")
-            import sympy as sp
-            from Clase.ejercicio2 import Ecuacion2
-            x = sp.Symbol("x")
-            y = sp.Function("y")
-            
-            sol2 = Ecuacion2(x,y,a)
-            print(sol2)
-            sol2.resolver()
+
 
         elif opcion == '3':
             print("Enunciado del ejercicio 3...\n")
-            import sympy as sp
-            from Clase.ejercicio3 import Ecuacion3
-            x = sp.Symbol("x")
-            y = sp.Function("y")
-            
-            sol3 = Ecuacion3(x,y,a)
-            print(sol3)
-            sol3.resolver()
+
 
         
         elif opcion == '4':
-            print("Saliendo de la clase de ecuaciones...\n")
+            print("Saliendo del programa...\n")
             break
 
         input("\nPresiona ENTER para continuar...")
         
-iniciar()
